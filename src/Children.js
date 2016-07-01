93f0c5492409ea47897939a2bdd0c788e3d8c717
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2016-07-01 23:43:04 +0600

    Nothing special

---
+++ src/Children.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+    render() {
+        return (
+            <h2>Another component!</h2>
+        );
+    }
+}
